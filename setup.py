2651ed39a0173697684e817824b2fc6b2e2c00a2
Erik <erik@norsar.no>
2021-10-26 11:13:57 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 
-from setuptools import setup
+import setuptools
 
-setup(
-   name='nais',
-   version='1.0',
-   description='NORSAR AI System',
-   author='NORSAR',
-   author_email='erik@norsar.bo',
-   packages=['nais'],
+with open("README.md") as file:
+    read_me_description = file.read()
+
+setuptools.setup(
+    name="nais",
+    version="0.1",
+    author="Erik B. Myklebust",
+    author_email="erik@norsar.no",
+    description="NORSAR AI System.",
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    url="http://bitbucket:7990/projects/GEOB/repos/nais",
+    packages=[''],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
