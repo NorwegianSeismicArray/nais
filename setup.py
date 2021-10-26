f297e3e0f7d5f1dd48ce4a63cf006ff9b14351a4
Erik <erik@norsar.no>
2021-10-26 10:42:56 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+   name='nais',
+   version='1.0',
+   description='NORSAR AI System',
+   author='NORSAR',
+   author_email='erik@norsar.bo',
+   packages=['nais'],
+)
