058803b9138af64a43cc971d83164b5f09c34955
Erik <erik@norsar.no>
2021-11-02 09:52:07 +0100

    Added get_config to layers

--- nais/Augment.py
+++ nais/Augment.py
@@ -8,13 +8,17 @@ class RandomCrop1D(tf.keras.layers.Layer):
     crop :: float, proportion to crop. default 0.1.
     name :: str
     """
+
     def __init__(self, crop=0.1, name='RandomCrop1D'):
         super(RandomCrop1D, self).__init__(name=name)
         self.crop = crop
 
+    def get_config(self):
+        return dict(crop=self.crop, name=self.name)
+
     def build(self, input_dim):
         _, x_size, y_size = input_dim
-        self.length = int(x_size*(1-self.crop))
+        self.length = int(x_size * (1 - self.crop))
         self.channels = y_size
         self.rc_layer = tf.keras.layers.RandomCrop(self.length, self.channels)
 
@@ -32,6 +36,9 @@ class SpectrogramTimeAugment(tf.keras.layers.Layer):
         super(SpectrogramTimeAugment, self).__init__(name=name)
         self.prop = prop
 
+    def get_config(self):
+        return dict(prop=self.prop, name=self.name)
+
     def call(self, inputs):
         _, length, _ = inputs.shape[1:]
         mask = tf.ones_like(inputs)
@@ -44,6 +51,9 @@ class SpectrogramFreqAugment(tf.keras.layers.Layer):
     def __init__(self, prop=0.1, name='SpectrogramFreqAugment'):
         super(SpectrogramFreqAugment, self).__init__(name=name)
         self.prop = prop
+
+    def get_config(self):
+        return dict(prop=self.prop, name=self.name)
 
     def call(self, inputs):
         height, _, _ = inputs.shape[1:]
--- nais/Layers.py
+++ nais/Layers.py
@@ -26,6 +26,16 @@ class StackedSpectrogram(tf.keras.layers.Layer):
         self.num_components = num_components
         self.stack_method = stack_method
 
+
+    def get_config(self):
+        return dict(n_fft = self.n_fft,
+                win_length = self.win_length,
+                hop_length = self.hop_length,
+                output_dim = self.output_dim,
+                num_components = self.num_components,
+                stack_method = self.stack_method,
+                name=self.name)
+
     def build(self,input_shape):
         inp = tf.keras.layers.Input(input_shape[1:])
         x = STFT(n_fft=self.n_fft, win_length=self.win_length, hop_length=self.hop_length)(inp)
@@ -53,14 +63,29 @@ class ResidualConv1D(keras.layers.Layer):
     def __init__(self, filters=32, kernel_size=3, stacked_layer=1):
         super(ResidualConv1D, self).__init__()
 
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.stacked_layer = stacked_layer
+
+    def build(self, input_shape):
         self.sigmoid_layers = []
         self.tanh_layers = []
         self.conv_layers = []
 
-        for dilation_rate in [2 ** i for i in range(stacked_layer)]:
-            self.sigmoid_layers.append(keras.layers.Conv1D(filters, kernel_size, dilation_rate=dilation_rate, padding='same', activation='sigmoid'))
-            self.tanh_layers.append(keras.layers.Conv1D(filters, kernel_size, dilation_rate=dilation_rate, padding='same', activation='tanh'))
-            self.conv_layers.append(keras.layers.Conv1D(filters, 1, padding='same'))
+        for dilation_rate in [2 ** i for i in range(self.stacked_layer)]:
+            self.sigmoid_layers.append(
+                keras.layers.Conv1D(self.filters, self.kernel_size, dilation_rate=dilation_rate, padding='same',
+                                    activation='sigmoid'))
+            self.tanh_layers.append(
+                keras.layers.Conv1D(self.filters, self.kernel_size, dilation_rate=dilation_rate, padding='same',
+                                    activation='tanh'))
+            self.conv_layers.append(keras.layers.Conv1D(self.filters, 1, padding='same'))
+
+    def get_config(self):
+        return dict(name=self.name,
+                    filters=self.filters,
+                    kernel_size=self.kernel_size,
+                    stacked_layer=self.stacked_layer)
 
     def call(self, inputs):
         residual_output = inputs
@@ -80,14 +105,25 @@ class ResidualConv1DTranspose(keras.layers.Layer):
     def __init__(self, filters=32, kernel_size=3, stacked_layer=1):
         super(ResidualConv1DTranspose, self).__init__()
 
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.stacked_layer = stacked_layer
+
+    def build(self, input_shape):
         self.sigmoid_layers = []
         self.tanh_layers = []
         self.conv_layers = []
 
-        for dilation_rate in [2 ** i for i in range(stacked_layer)]:
-            self.sigmoid_layers.append(keras.layers.Conv1DTranspose(filters, kernel_size, dilation_rate=dilation_rate, padding='same', activation='sigmoid'))
-            self.tanh_layers.append(keras.layers.Conv1DTranspose(filters, kernel_size, dilation_rate=dilation_rate, padding='same', activation='tanh'))
-            self.conv_layers.append(keras.layers.Conv1DTranspose(filters, 1, padding='same'))
+        for dilation_rate in [2 ** i for i in range(self.stacked_layer)]:
+            self.sigmoid_layers.append(keras.layers.Conv1DTranspose(self.filters, self.kernel_size, dilation_rate=dilation_rate, padding='same', activation='sigmoid'))
+            self.tanh_layers.append(keras.layers.Conv1DTranspose(self.filters, self.kernel_size, dilation_rate=dilation_rate, padding='same', activation='tanh'))
+            self.conv_layers.append(keras.layers.Conv1DTranspose(self.filters, 1, padding='same'))
+
+    def get_config(self):
+        return dict(name=self.name,
+                    filters=self.filters,
+                    kernel_size=self.kernel_size,
+                    stacked_layer=self.stacked_layer)
 
     def call(self, inputs):
         residual_output = inputs
