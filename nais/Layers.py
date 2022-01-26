d6c4ad1dd797ec05d26cc55286d341921d9a8c8a
Erik <erik@norsar.no>
2022-01-26 09:40:42 +0100

    Added Resampling1D layer

--- nais/Layers.py
+++ nais/Layers.py
@@ -144,12 +144,14 @@ class Resampling1D(tf.keras.layers.Layer):
     def build(self, input_shape):
         self.ls = tf.keras.layers.Resizing(self.length, input_shape[-1], interpolation=self.interpolation)
 
-    def output_shape(self, input_shape):
+    def compute_output_shape(input_shape):
         return (self.length, input_shape[-1])
 
     def call(self,inputs):
-        inputs = tf.expand_dims(inputs,axis=-1)
-        return tf.squeeze(self.ls(inputs))
+        x = tf.expand_dims(inputs,axis=-1)
+        x = self.ls(x)
+        print(x)
+        return tf.squeeze(x)
 
     def get_config(self):
         return dict(name=self.name, length=self.length)
