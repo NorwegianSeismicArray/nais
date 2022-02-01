58d0849a7a98e14f5062f23c6c0ca88959e3a39a
Erik <erik@norsar.no>
2022-02-01 10:18:31 +0100

    Added CosSimConv1D and MaxAbsPool1D layers"

--- nais/Layers.py
+++ nais/Layers.py
@@ -151,3 +151,132 @@ class Resampling1D(tf.keras.layers.Layer):
 
     def get_config(self):
         return dict(name=self.name, length=self.length)
+
+
+class CosSimConv1D(tf.keras.layers.Layer):
+    def __init__(self, units=32, kernel_size=3):
+        super(CosSimConv1D, self).__init__()
+        self.units = units
+        self.kernel_size = kernel_size
+
+    def build(self, input_shape):
+        self.in_shape = input_shape
+
+        self.flat_size = self.in_shape[1]
+        self.channels = self.in_shape[2]
+
+        self.w = self.add_weight(
+            shape=(1, self.channels * tf.square(self.kernel_size), self.units),
+            initializer="glorot_uniform",
+            trainable=True,
+        )
+        self.b = self.add_weight(
+            shape=(self.units,), initializer="zeros", trainable=True)
+
+        self.p = self.add_weight(
+            shape=(self.units,), initializer='ones', trainable=True)
+
+        self.q = self.add_weight(
+            shape=(1,), initializer='zeros', trainable=True)
+
+    def l2_normal(self, x, axis=None, epsilon=1e-12):
+        square_sum = tf.reduce_sum(tf.square(x), axis, keepdims=True)
+        x_inv_norm = tf.sqrt(tf.maximum(square_sum, epsilon))
+        return x_inv_norm
+
+    def stack3(self, x):
+        x = tf.stack(
+            [
+                tf.pad(x[:, :-1, :], tf.constant([[0, 0], [1, 0], [0, 0]])),
+                x,
+                tf.pad(x[:, :-1, :], tf.constant([[0, 0], [0, 1], [0, 0]])),
+
+                tf.pad(x[:, :-1, :], tf.constant([[0, 0], [1, 0], [0, 0]])),
+                x,
+                tf.pad(x[:, 1:, :], tf.constant([[0, 0], [0, 1], [0, 0]])),
+
+                tf.pad(x[:, 1:, :], tf.constant([[0, 0], [1, 0], [0, 0]])),
+                x,
+                tf.pad(x[:, 1:, :], tf.constant([[0, 0], [0, 1], [0, 0]])),
+
+            ], axis=2)
+        return x
+
+    def call(self, inputs, training=None):
+        x = self.stack3(inputs)
+        x = tf.reshape(x, (-1, self.flat_size, self.channels * tf.square(self.kernel_size)))
+        q = tf.square(self.q)
+        x_norm = self.l2_normal(x, axis=2) + q
+        w_norm = self.l2_normal(self.w, axis=1) + q
+        sign = tf.sign(tf.matmul(x, self.w))
+        x = tf.matmul(x / x_norm, self.w / w_norm)
+        x = tf.abs(x) + 1e-12
+        x = tf.pow(x, tf.square(self.p))
+        x = sign * x + self.b
+        x = tf.reshape(x, (-1, self.in_shape[1], self.units))
+        return x
+
+
+class MaxAbsPool1D(tf.keras.layers.Layer):
+    def __init__(self, pool_size, pad_to_fit=False):
+        super(MaxAbsPool1D, self).__init__()
+        self.pad = pad_to_fit
+        self.pool_size = pool_size
+
+    def compute_output_shape(self, in_shape):
+        if self.pad:
+            return (in_shape[0],
+                    tf.math.ceil(in_shape[1] / self.pool_size),
+                    in_shape[2])
+        return (in_shape[0],
+                (in_shape[1] // self.pool_size),
+                in_shape[2])
+
+    def compute_padding(self, in_shape):
+        mod_y = in_shape[1] % self.pool_size
+        y1 = mod_y // 2
+        y2 = mod_y - y1
+        self.padding = ((0, 0), (y1, y2), (0, 0))
+
+    def build(self, input_shape):
+        self.in_shape = input_shape
+        self.out_shape = self.compute_output_shape(self.in_shape)
+        self.compute_padding(self.in_shape)
+
+    @tf.function
+    def stack(self, inputs):
+        if self.pad:
+            inputs = tf.pad(inputs, self.padding)
+        batch_size = tf.shape(inputs)[0]
+        max_height = (tf.shape(inputs)[1] // self.pool_size) * self.pool_size
+        stack = tf.stack(
+            [inputs[:, i:max_height:self.pool_size, :]
+             for i in range(self.pool_size)],
+            axis=-1)
+        return stack
+
+    @tf.function
+    def call(self, inputs, training=None):
+        stacked = self.stack(inputs)
+        inds = tf.argmax(tf.abs(stacked), axis=-1, output_type=tf.int32)
+        ks = tf.shape(stacked)
+        idx = tf.stack([
+            *tf.meshgrid(
+                tf.range(0, ks[0]),
+                tf.range(0, ks[1]),
+                tf.range(0, ks[2]),
+                indexing='ij'
+            ), inds],
+            axis=-1)
+
+        x = tf.gather_nd(stacked, idx)
+        x = tf.reshape(x, (-1, *self.out_shape[1:]))
+        return x
+
+
+
+
+
+
+
+
