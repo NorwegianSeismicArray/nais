428b0fe340b711e6409be94849c2928da41b876a
Erik <erik@norsar.no>
2022-05-09 13:12:20 +0200

    testing new shift

--- nais/Datareader.py
+++ nais/Datareader.py
@@ -239,8 +239,9 @@ class AugmentWaveformSequence(tf.keras.utils.Sequence):
 
     def _shift_crop(self, img, mask, detection):
         start, end = detection
-        y = int(np.random.uniform(0, int(len(img) - self.new_length)))
-        return img[y:y+self.new_length], mask[y:y+self.new_length]
+        middle = (end - start) / 2
+        y = int(np.random.uniform(self.new_length//2, self.new_length//2))
+        return img[-y+middle:middle+y], mask[-y+middle:middle+y]
 
     def _taper(self, img, mask, alpha=0.1):
         w = tukey(img.shape[0], alpha)
