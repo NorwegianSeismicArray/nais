14d186466e04c1b6a4aa06edddc1c1c08db1430f
Erik <erik@norsar.no>
2022-11-24 09:56:26 +0100

    Added TransPhaseNet

--- nais/Models.py
+++ nais/Models.py
@@ -742,6 +742,137 @@ class EarthQuakeTransformerMetadata(EarthQuakeTransformer):
         m = self.metadata_model(encoded)
         return d, p, s, m
 
+class TransPhaseNet(tf.keras.Model):
+
+    def __init__(self,
+                 num_classes=2,
+                 filters=None,
+                 output_activation='linear',
+                 kernel_regularizer=None,
+                 dropout_rate=0.2,
+                 transformer_size=64,
+                 initializer='glorot_normal',
+                 name='PhaseNet'):
+        super(PhaseNet, self).__init__(name=name)
+        self.num_classes = num_classes
+        self.initializer = initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.dropout_rate = dropout_rate
+        self.output_activation = output_activation
+        self.transformer_size = transformer_size
+
+        if filters is None:
+            self.filters = [4, 8, 16, 32]
+        else:
+            self.filters = filters
+
+    def build(self, input_shape):
+        inputs = tf.keras.Input(shape=input_shape[1:])
+
+        ### [First half of the network: downsampling inputs] ###
+
+        def block_transformer(f, width, x):
+            att, w = SeqSelfAttention(return_attention=True,
+                                      attention_width=width)(x)
+            att = tfl.Add()([x, att])
+            norm = tfl.LayerNormalization()(att)
+            ff = tf.keras.Sequential([tfl.Dense(f, activation='relu', kernel_regularizer=kernel_regularizer),
+                                      tfl.Dropout(dropout),
+                                      tfl.Dense(norm.shape[2]),
+                                      ])(norm)
+            ff_add = tfl.Add()([norm, ff])
+            norm_out = tfl.LayerNormalization()(ff_add)
+            return norm_out, w
+
+        # Entry block
+        x = tfl.Conv1D(self.filters[0], 7,
+                       strides=2,
+                       kernel_regularizer=self.kernel_regularizer,
+                       padding="same",
+                       name='entry')(inputs)
+
+        x = tfl.BatchNormalization()(x)
+        x = tfl.Activation("relu")(x)
+        x = tfl.Dropout(self.dropout_rate)(x)
+
+        previous_block_activation = x  # Set aside residual
+
+        # Blocks 1, 2, 3 are identical apart from the feature depth.
+        for i, filters in enumerate(self.filters[1:]):
+            x = tfl.Activation("relu")(x)
+            x = tfl.Conv1D(filters, 7, padding="same",
+                           kernel_regularizer=self.kernel_regularizer,
+                           kernel_initializer=self.initializer,
+                           )(x)
+            x = tfl.BatchNormalization()(x)
+            x = tfl.Activation("relu")(x)
+            x = tfl.Dropout(self.dropout_rate)(x)
+
+            x = tfl.Conv1D(filters, 7, padding="same",
+                           kernel_regularizer=self.kernel_regularizer,
+                           kernel_initializer=self.initializer,
+                           )(x)
+            x = tfl.BatchNormalization()(x)
+            x, _ = block_transformer(self.transformer_size, None, x)
+
+            x = tfl.MaxPooling1D(4, strides=2, padding="same")(x)
+
+            # Project residual
+            residual = tfl.Conv1D(filters, 1, strides=2, padding="same", kernel_initializer=self.initializer,
+                                  )(
+                previous_block_activation
+            )
+            x = tfl.concatenate([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        self.encoder = tf.keras.Model(inputs, x)
+        ### [Second half of the network: upsampling inputs] ###
+
+        for filters in self.filters[::-1]:
+            x = tfl.Activation("relu")(x)
+            x = tfl.Conv1DTranspose(filters, 7, padding="same",
+                                    kernel_regularizer=self.kernel_regularizer,
+                                    kernel_initializer=self.initializer,
+                                    )(x)
+            x = tfl.BatchNormalization()(x)
+            x = tfl.Activation("relu")(x)
+            x = tfl.Dropout(self.dropout_rate)(x)
+
+            x = tfl.Conv1DTranspose(filters, 7, padding="same",
+                                    kernel_regularizer=self.kernel_regularizer,
+                                    kernel_initializer=self.initializer,
+                                    )(x)
+            x = tfl.BatchNormalization()(x)
+            x = tfl.UpSampling1D(2)(x)
+
+            # Project residual
+            residual = tfl.UpSampling1D(2)(previous_block_activation)
+            residual = tfl.Conv1D(filters, 1, padding="same",
+                                  kernel_regularizer=self.kernel_regularizer,
+                                  kernel_initializer=self.initializer,
+                                  )(residual)
+            x = tfl.concatenate([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
+
+        # Add a per-pixel classification layer
+        if self.num_classes is not None:
+            outputs = tfl.Conv1D(self.num_classes,
+                                 3,
+                                 padding="same",
+                                 activation=self.output_activation)(x)
+        else:
+            outputs = x
+
+        # Define the model
+        self.model = tf.keras.Model(inputs, outputs)
+
+    def summary(self):
+        return self.model.summary()
+
+    def call(self, inputs):
+        return self.model(inputs)
+
+
 class ScatNet(tf.keras.Model):
     def __init__(self,
                  input_shape,
