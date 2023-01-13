40bb438346645925c63995ab24403a6b3cf6b0cc
Erik <erik.bryhn.myklebust@gmail.com>
2023-01-13 14:51:24 +0100

    updated

--- nais/Augment/__init__.py
+++ nais/Augment/__init__.py
@@ -1,3 +1,3 @@
 
-from .crop import *
-from .spectrogram import *
+from .crop import RandomCrop1D
+from .spectrogram import SpectrogramFreqAugment, SpectrogramTimeAugment
--- nais/Layers/__init__.py
+++ nais/Layers/__init__.py
@@ -1,9 +1,9 @@
 
-from .attention import *
-from .cossim import * 
-from .feedforward import * 
-from .pooling import * 
-from .resampling import *
-from .residual import * 
-from .scatter import * 
-from .spectrogram import *
+from .attention import SeqSelfAttention
+from .cossim import CosSimConv1D
+from .feedforward import FeedForward
+from .pooling import MaxAbsPool1D 
+from .resampling import Resampling1D
+from .residual import ResidualConv1D, ResidualConv1DTranspose, ResnetBlock1D 
+from .scatter import Scattering
+from .spectrogram import StackedSpectrogram
