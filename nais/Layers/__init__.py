20115753591bd305e85b2e8ca86248dafeb3b84e
Erik <erik.bryhn.myklebust@gmail.com>
2023-01-13 14:31:15 +0100

    Updated

--- nais/Layers/__init__.py
+++ nais/Layers/__init__.py
@@ -1,9 +1,9 @@
 
-from attention import *
-from cossim import * 
-from feedforward import * 
-from pooling import * 
-from resampling import *
-from residual import * 
-from scatter import * 
-from spectrogram import *
+from .attention import *
+from .cossim import * 
+from .feedforward import * 
+from .pooling import * 
+from .resampling import *
+from .residual import * 
+from .scatter import * 
+from .spectrogram import *
