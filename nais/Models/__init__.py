e4323606118fd1a2a61b9092588fcfe71c0706e6
Erik <erik.bryhn.myklebust@gmail.com>
2023-01-16 08:04:16 +0100

    updated init

--- nais/Models/__init__.py
+++ nais/Models/__init__.py
@@ -2,8 +2,8 @@
 from .alexnet import AlexNet1D, AlexNet2D
 from .autoencoder import WaveAutoEncoder, ImageAutoEncoder
 from .eqtransformer import EarthQuakeTransformer, EarthQuakeTransformerMetadata
-from .phasenet import PhaseNet, PhaseNetMetadata, TransPhaseNet, TransPhaseNetMetadata, TransPhaseNetV2
+from .phasenet import PhaseNet, PhaseNetMetadata, TransPhaseNet, TransPhaseNetMetadata, TransPhaseNetV2, TransPhaseNetV2Metadata
 from .scatnet import ScatNet
 from .spectrogram import CreateSpectrogramModel 
-from .utime import UTime 
-from .wavenet import WaveNet 
+from .utime import UTime
+from .wavenet import WaveNet
