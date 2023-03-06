970f394af85ebc53220d271482b78916c6c8afa1
Erik <erik.bryhn.myklebust@gmail.com>
2023-03-06 10:17:59 +0100

    Added more models

--- nais/Models/phasenet.py
+++ nais/Models/phasenet.py
@@ -475,3 +475,158 @@ class DynamicPhaseNetMetadata(DynamicPhaseNet):
         m = self.encoder(inputs)
         return p, self.metadata_model(m)
     
+
+from nais.Layers import ResidualConv1D
+
+class DilatedPhaseNet(tf.keras.Model):
+    def __init__(self,
+                 num_classes=2,
+                 filters=None,
+                 kernelsizes=None,
+                 num_stacked_layers=3,
+                 output_activation='linear',
+                 kernel_regularizer=None,
+                 dropout_rate=0.2,
+                 pool_type='max',
+                 activation='relu',
+                 initializer='glorot_normal',
+                 name='DilatedPhaseNet'):
+        """Adapted to 1D from https://keras.io/examples/vision/oxford_pets_image_segmentation/
+
+        Args:
+            num_classes (int, optional): number of outputs. Defaults to 2.
+            filters (list, optional): list of number of filters. Defaults to None.
+            kernelsizes (list, optional): list of kernel sizes. Defaults to None.
+            output_activation (str, optional): output activation, eg., 'softmax' for multiclass problems. Defaults to 'linear'.
+            kernel_regularizer (tf.keras.regualizers.Regualizer, optional): kernel regualizer. Defaults to None.
+            dropout_rate (float, optional): dropout. Defaults to 0.2.
+            initializer (tf.keras.initializers.Initializer, optional): weight initializer. Defaults to 'glorot_normal'.
+            name (str, optional): model name. Defaults to 'PhaseNet'.
+        """
+        super(DilatedPhaseNet, self).__init__(name=name)
+        self.num_classes = num_classes
+        self.initializer = initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.dropout_rate = dropout_rate
+        self.output_activation = output_activation
+        self.activation = activation
+        self.num_stacked_layers = num_stacked_layers
+
+        if filters is None:
+            self.filters = [4, 8, 16, 32]
+        else:
+            self.filters = filters
+
+        if kernelsizes is None:
+            self.kernelsizes = [7, 7, 7, 7]
+        else:
+            self.kernelsizes = kernelsizes
+            
+        if pool_type == 'max':
+            self.pool_layer = tfl.MaxPooling1D
+        else:
+            self.pool_layer = tfl.AveragePooling1D
+            
+    def _down_block(self, f, ks, x):
+        x = ResidualConv1D(f, 
+                          ks,
+                          stacked_layer=self.num_stacked_layers)(x)
+        x = self.pool_layer(4, strides=2, padding="same")(x)
+        return x
+    
+    def _up_block(self, f, ks, x):
+        x = ResidualConv1D(f, 
+                        ks,
+                        stacked_layer=self.num_stacked_layers)(x)
+        x = tfl.UpSampling1D(2)(x)
+        return x
+        
+    def build(self, input_shape):
+        inputs = tf.keras.Input(shape=input_shape[1:])
+
+        ### [First half of the network: downsampling inputs] ###
+
+        # Entry block
+        x = tfl.Conv1D(self.filters[0], 
+                       self.kernelsizes[0],
+                       kernel_regularizer=self.kernel_regularizer,
+                       padding="same")(x)
+
+        skips = [x]
+        
+        # Blocks 1, 2, 3 are identical apart from the feature depth.
+        for i, (f, ks) in enumerate(zip(self.filters, self.kernelsizes)):
+            x = self._down_block(f, ks, x)
+            skips.append(x)
+            
+        skips = skips[:-1]
+
+        self.encoder = tf.keras.Model(inputs, x)
+        ### [Second half of the network: upsampling inputs] ###
+        skips = skips[::-1]
+        
+        for i, (f, ks) in enumerate(zip(self.filters[::-1], self.kernelsizes[::-1])):
+            x = self._up_block(f, ks, x)
+            x = crop_and_concat(x, skips[i])
+
+        to_crop = x.shape[1] - input_shape[1]
+        of_start, of_end = to_crop // 2, to_crop // 2
+        of_end += to_crop % 2
+        x = tfl.Cropping1D((of_start, of_end))(x)
+        
+        #Exit block
+        x = tfl.Conv1D(self.filters[0], 
+                       self.kernelsizes[0],
+                       strides=1,
+                       kernel_regularizer=self.kernel_regularizer,
+                       padding="same",
+                       name='exit')(x)
+
+        x = tfl.BatchNormalization()(x)
+        x = tfl.Activation(self.activation)(x)
+        x = tfl.Dropout(self.dropout_rate)(x)
+
+        # Add a per-pixel classification layer
+        if self.num_classes is not None:
+            x = tfl.Conv1D(self.num_classes,
+                           1,
+                           padding="same")(x)
+            outputs = tfl.Activation(self.output_activation, dtype='float32')(x)
+        else:
+            outputs = x
+
+        # Define the model
+        self.model = tf.keras.Model(inputs, outputs)
+
+    @property
+    def num_parameters(self):
+        return sum([np.prod(K.get_value(w).shape) for w in self.model.trainable_weights])
+
+    def summary(self):
+        return self.model.summary()
+
+    def call(self, inputs):
+        return self.model(inputs)
+    
+    
+class DilatedPhaseNetMetadata(DynamicPhaseNet):
+    def __init__(self, num_outputs=None, metadata_model=None, ph_kw=None):
+        """Provides a wrapper for PhaseNet with a metadata output, eg., when learning back azimuth.
+
+        Args:
+            num_outputs (int): Number of numerical metadata to learn. 
+            ph_kw (dict): Args. for PhaseNet. 
+        """
+        super(DilatedPhaseNetMetadata, self).__init__(**ph_kw)
+        if metadata_model is None:
+            self.metadata_model = tf.keras.Sequential([tfl.Flatten(),
+                                                   tfl.Dense(128, activation='relu'),
+                                                   tfl.Dense(num_outputs)])
+        else:
+            self.metadata_model = metadata_model
+            
+    def call(self, inputs):
+        p = self.model(inputs)
+        m = self.encoder(inputs)
+        return p, self.metadata_model(m)
+    
