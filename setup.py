05a8092c5ede0f75aa4877c1b18397dcfe894eee
Tord Sture Stangeland <tord.stangeland@gmail.com>
2023-11-03 15:09:25 +0100

    Version update

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 import setuptools
 from setuptools import find_packages
 
-with open("./README.md") as file:
+with open("/staff/tord/Workspace/nais/README.md") as file:
     read_me_description = file.read()
 
 setuptools.setup(
     name="nais",
-    version="1.2",
-    author="Erik B. Myklebust",
-    author_email="erik@norsar.no",
+    version="1.4",
+    author="Erik B. Myklebust & Tord S. Stangeland",
+    author_email="tord@norsar.no",
     description="NORSAR AI System.",
     long_description=read_me_description,
     long_description_content_type="text/markdown",
